354ea400be3282cd67972cae9d7d8d01a4e435f6
J. Alejandro Carrillo <49215772+TheLizardKing55@users.noreply.github.com>
2024-11-27 17:56:26 -0600

    Update ibmsecurityverifydirectory_controller.go
    To give the operator the necessary permissions to work with the newly introduced resources.

--- src/controllers/ibmsecurityverifydirectory_controller.go
+++ src/controllers/ibmsecurityverifydirectory_controller.go
@@ -56,6 +56,7 @@ type IBMSecurityVerifyDirectoryReconciler struct {
 //+kubebuilder:rbac:groups=ibm.com,resources=ibmsecurityverifydirectories/status,verbs=get;update;patch
 //+kubebuilder:rbac:groups=ibm.com,resources=ibmsecurityverifydirectories/finalizers,verbs=update
 //+kubebuilder:rbac:groups=apps,resources=deployments,verbs=get;list;watch;create;update;patch;delete
+//+kubebuilder:rbac:groups=apps,resources=replicasets,verbs=get;list;watch;create;update;patch;delete
 //+kubebuilder:rbac:groups=core,resources=pods,verbs=get;list;watch;create;delete
 //+kubebuilder:rbac:groups=core,resources=pods/exec,verbs=create
 //+kubebuilder:rbac:groups=core,resources=services,verbs=get;list;watch;create;delete
